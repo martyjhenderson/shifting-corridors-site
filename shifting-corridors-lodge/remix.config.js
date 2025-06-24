a31b9a4cddf6549820bc1c5ac41ebfd120fb781a
Marty Henderson <marty@ravegrunt.is>
2025-06-23 22:45:53 -0500

    K remixes

---
+++ shifting-corridors-lodge/remix.config.js
@@ -0,0 +1,9 @@
+/** @type {import('@remix-run/dev').AppConfig} */
+module.exports = {
+  ignoredRouteFiles: ["**/.*"],
+  appDirectory: "src",
+  assetsBuildDirectory: "build/assets",
+  publicPath: "/assets/",
+  serverBuildPath: "build/index.js",
+  serverModuleFormat: "cjs",
+};
