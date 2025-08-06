f94fa6e0e3d79f2eed523a77397dc7d71c860a18
Marty Henderson <marty@ravegrunt.is>
2025-08-06 14:17:59 -0500

    Kiro adds Starfinder on 27 Aug

--- shifting-corridors-lodge/src/utils/markdown/markdownUtils.ts
+++ shifting-corridors-lodge/src/utils/markdown/markdownUtils.ts
@@ -428,16 +428,16 @@ Please register in advance using the links above. Space is limited, so sign up e
       },
       {
         meta: {
-          title: 'Pathfinder Society at Diversions - August 27',
+          title: 'Pathfinder & Starfinder Society at Diversions - August 27',
           date: '2025-08-27',
           url: '/events/diversions-aug-27-2025',
           location: 'Diversions',
           address: '119 2nd St #300, Coralville, IA 52241'
         },
         content: `
-# Pathfinder Society at Diversions
+# Pathfinder & Starfinder Society at Diversions
 
-Join us for Pathfinder Society games at Diversions in Coralville!
+Join us for Pathfinder and Starfinder Society games at Diversions in Coralville!
 
 ## Details
 
@@ -448,11 +448,15 @@ Join us for Pathfinder Society games at Diversions in Coralville!
 
 ## Available Scenarios
 
+### Pathfinder Society
 1. **United in Purpose** (Levels 1-2) - [Sign up here](https://www.rpgchronicles.net/session/ad88f0d0-065c-4edc-a79b-92d9377ac1d3/pregame)
+
+### Starfinder Society
+1. **Mystery of the Frozen Moon** (Levels 1-2, 5:30 PM - 8:30 PM) - [Sign up here](https://www.rpgchronicles.net/session/eb62c1ba-2514-4981-9c95-b3250b4ac763/pregame)
 
 ## Registration
 
-Please register in advance using the link above. Space is limited, so sign up early!
+Please register in advance using the links above. Space is limited, so sign up early!
         `,
         slug: 'diversions-aug-27-2025'
       }
