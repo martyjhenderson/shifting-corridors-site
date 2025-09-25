7eeb793109f624967e5a84b10daf682d4736d766
Marty Henderson <marty@ravegrunt.is>
2025-09-24 22:46:14 -0500

    Tempest Games Quest on 2 Oct

--- shifting-corridors-lodge/src/utils/markdown/markdownUtils.ts
+++ shifting-corridors-lodge/src/utils/markdown/markdownUtils.ts
@@ -654,6 +654,37 @@ Join us for Pathfinder and Starfinder Society games at Diversions in Coralville!
 Please register in advance using the links above. Space is limited, so sign up early!
         `,
         slug: 'diversions-sep-24-2025'
+      },
+      {
+        meta: {
+          title: 'Pathfinder Society at Tempest Games - The Winter Queen\'s Dollhouse',
+          date: '2025-10-02',
+          url: '/events/tempest-winter-queen-dollhouse',
+          location: 'Tempest Games',
+          address: '212 Edgewood Road NW, Suite K, Cedar Rapids, IA 52405'
+        },
+        content: `
+# Pathfinder Society at Tempest Games
+
+Join us for Pathfinder Society games at Tempest Games in Cedar Rapids!
+
+## Details
+
+- **Date:** October 2, 2025
+- **Time:** 5:30 PM - 7:30 PM Central Time
+- **Location:** Tempest Games
+- **Address:** 212 Edgewood Road NW, Suite K, Cedar Rapids, IA 52405
+- **Players:** 6 players
+
+## Available Scenarios
+
+1. **The Winter Queen's Dollhouse** - [Sign up here](https://www.rpgchronicles.net/session/22928d9b-3beb-4b6c-ad50-e7a60c8142cc/pregame)
+
+## Registration
+
+Please register in advance using the link above. Space is limited to 6 players, so sign up early!
+        `,
+        slug: 'tempest-winter-queen-dollhouse'
       }
     ];
   } else if (directory === 'news') {
